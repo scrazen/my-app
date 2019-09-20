5c2a720688eb56b936623169baf6fbc9e0a423ee
Marshall Westfall <scrazen@gmail.com>
2019-09-20 14:42:09 -0500

    Updates

---
+++ precache-manifest.2e49a4adbde4c8d4200c986759458a45.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "628a333e3ad5a457a4e2f1768ba8f445",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "41be49d1d74d69f09fac",
+    "url": "/my-app/static/css/main.d82baeb4.chunk.css"
+  },
+  {
+    "revision": "7f5ba2d2b19b3516ee37",
+    "url": "/my-app/static/js/2.f6529390.chunk.js"
+  },
+  {
+    "revision": "41be49d1d74d69f09fac",
+    "url": "/my-app/static/js/main.31f49ab7.chunk.js"
+  },
+  {
+    "revision": "9c47355d93f1cffa1d27",
+    "url": "/my-app/static/js/runtime~main.eed5ce82.js"
+  }
+]);
